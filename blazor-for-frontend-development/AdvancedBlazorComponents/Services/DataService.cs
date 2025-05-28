93fadb21c60dc15da633313d0401ba0adfa4420b
Mikael Færøvik <mikael.farovik@gmail.com>
2025-05-28 11:56:01 +0200

    Complete "Implementing Dependency Injection"

---
+++ blazor-for-frontend-development/AdvancedBlazorComponents/Services/DataService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+class DataService
+{
+    public List<string> GetData()
+    {
+        // Simulate fetching data from a database or an API
+        return new List<string> { "Data1", "Data2", "Data3" };
+    }
+}

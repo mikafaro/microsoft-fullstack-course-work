25f732b8fdf5d562385460c908e5689956ecf6c9
Mikael Færøvik <mikael.farovik@gmail.com>
2025-06-25 15:33:46 +0200

    EventEase - add userdetails model

---
+++ blazor-for-frontend-development/EventEaseApp/Models/UserDetails.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApp.Models
+{
+    public class UserDetails
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
